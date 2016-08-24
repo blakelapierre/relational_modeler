c9afb238b642fbec0134b9f5f46367ca1bb1f0da
Blake La Pierre <blakelapierre@gmail.com>
2016-08-23 18:27:15 -0700

    No immediate plans to implement for other databases, but I want to keep the specifics as only parameters

--- .dist/index.js
+++ .dist/index.js
@@ -13,9 +13,9 @@ var _orderTables = require('./transformers/orderTables');
 
 var _orderTables2 = _interopRequireDefault(_orderTables);
 
-var _toPostgreSQL2 = require('./transformers/postgreSQL/toPostgreSQL');
+var _toPostgreSQL = require('./transformers/postgreSQL/toPostgreSQL');
 
-var _toPostgreSQL3 = _interopRequireDefault(_toPostgreSQL2);
+var _toPostgreSQL2 = _interopRequireDefault(_toPostgreSQL);
 
 var _ohmLoader = require('./ohmLoader');
 
@@ -27,33 +27,51 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 require('./traceur-runtime');
 
-var _loadGrammarWithSeman = (0, _ohmLoader.loadGrammarWithSemantics)('RM_PGSQL', ['toObject'], _RMOhm2.default);
-
-var grammar = _loadGrammarWithSeman.grammar;
-var semantics = _loadGrammarWithSeman.semantics;
-
-
 var i = 2;
 if (process.argv[0].endsWith('relational_modeler')) i = 1;
 
-var fileName = process.argv[i],
-    defaultName = fileName.split('.').slice(0, -1).join('.').split(/[\\/]/).pop(); //http://stackoverflow.com/questions/3820381/need-a-basename-function-in-javascript#comment29942319_15270931
+var fileName = process.argv[i];
 
 var delimiter = process.argv[i + 1] || ',',
-    quote = process.argv[i + 2] || '"';
+    quote = process.argv[i + 2] || '"',
+    engineName = process.argv[i + 3] || 'postgresql';
 
-var model = (0, _ohmLoader.runFromFile)(fileName, grammar, semantics, 'toObject');
+var engines = {
+  'postgresql': {
+    name: 'RM_PGSQL',
+    grammarText: _RMOhm2.default,
+    transformer: _toPostgreSQL2.default
+  }
+};
 
-var _toPostgreSQL = (0, _toPostgreSQL3.default)((0, _orderTables2.default)(model), delimiter, quote);
+processModelFile(fileName, engines[engineName]);
 
-var schema = _toPostgreSQL.schema;
-var imports = _toPostgreSQL.imports;
+function processModelFile(fileName, _ref) {
+  var name = _ref.name;
+  var grammarText = _ref.grammarText;
+  var transformer = _ref.transformer;
+
+  var _loadGrammarWithSeman = (0, _ohmLoader.loadGrammarWithSemantics)(name, ['toObject'], grammarText);
+
+  var grammar = _loadGrammarWithSeman.grammar;
+  var semantics = _loadGrammarWithSeman.semantics;
 
 
-write(defaultName + '.sql', schema.join('\n') + '\n');
-write(defaultName + '.import', imports.join('\n') + '\n');
+  var model = (0, _ohmLoader.runFromFile)(fileName, grammar, semantics, 'toObject');
+
+  var _transformer = transformer((0, _orderTables2.default)(model), delimiter, quote);
+
+  var schema = _transformer.schema;
+  var imports = _transformer.imports;
+
+
+  var defaultName = fileName.split('.').slice(0, -1).join('.').split(/[\\/]/).pop(); //http://stackoverflow.com/questions/3820381/need-a-basename-function-in-javascript#comment29942319_15270931
+
+  write(defaultName + '.sql', schema.join('\n') + '\n');
+  write(defaultName + '.import', imports.join('\n') + '\n');
+}
 
 function write(fileName, content) {
-      console.log('writing', fileName);
-      _fs2.default.writeFileSync(fileName, content);
+  console.log('writing', fileName);
+  _fs2.default.writeFileSync(fileName, content);
 }
--- src/index.js
+++ src/index.js
@@ -12,23 +12,37 @@ import {loadGrammarWithSemantics, runFromFile} from './ohmLoader';
 
 import rm_pgsql_grammar from './grammar/RM.ohm.js';
 
-const {grammar, semantics} = loadGrammarWithSemantics('RM_PGSQL', ['toObject'], rm_pgsql_grammar);
-
 let i = 2;
 if (process.argv[0].endsWith('relational_modeler')) i = 1;
 
-const fileName = process.argv[i],
-      defaultName = fileName.split('.').slice(0, -1).join('.').split(/[\\/]/).pop(); //http://stackoverflow.com/questions/3820381/need-a-basename-function-in-javascript#comment29942319_15270931
+const fileName = process.argv[i];
 
 const delimiter = process.argv[i+1] || ',',
-      quote = process.argv[i+2] || '"';
+      quote = process.argv[i+2] || '"',
+      engineName = process.argv[i+3] || 'postgresql';
 
-const model = runFromFile(fileName, grammar, semantics, 'toObject');
+const engines = {
+  'postgresql': {
+    name: 'RM_PGSQL',
+    grammarText: rm_pgsql_grammar,
+    transformer: toPostgreSQL
+  }
+};
 
-const {schema, imports} = toPostgreSQL(orderTables(model), delimiter, quote);
+processModelFile(fileName, engines[engineName]);
 
-write(`${defaultName}.sql`, schema.join('\n') + '\n');
-write(`${defaultName}.import`, imports.join('\n') + '\n');
+function processModelFile(fileName, {name, grammarText, transformer}) {
+  const {grammar, semantics} = loadGrammarWithSemantics(name, ['toObject'], grammarText);
+
+  const model = runFromFile(fileName, grammar, semantics, 'toObject');
+
+  const {schema, imports} = transformer(orderTables(model), delimiter, quote);
+
+  const defaultName = fileName.split('.').slice(0, -1).join('.').split(/[\\/]/).pop(); //http://stackoverflow.com/questions/3820381/need-a-basename-function-in-javascript#comment29942319_15270931
+
+  write(`${defaultName}.sql`, schema.join('\n') + '\n');
+  write(`${defaultName}.import`, imports.join('\n') + '\n');
+}
 
 function write(fileName, content) {
   console.log('writing', fileName);
