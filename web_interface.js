55994b2e4fa96aebf64b362f6e9035315b1b3e89
Blake La Pierre <blakelapierre@gmail.com>
2016-09-03 22:09:16 -0700

    Add import method choice; import method scripts; switch usda model to native table names

--- web_interface.js
+++ web_interface.js
@@ -1,56 +1,42 @@
 'use strict';
 
-var _api2 = require('../api');
+var _orderTables = require('./transformers/orderTables');
 
-var delimiter = '^',
-    quote = '~';
-// [model, result, sql, import] = ["model", "result", "sql", "import"].map(n => document.getElementBy)
+var _orderTables2 = _interopRequireDefault(_orderTables);
 
-var currentEngine = 'postgresql';
+var _toPostgreSQL = require('./transformers/postgreSQL/toPostgreSQL');
+
+var _toPostgreSQL2 = _interopRequireDefault(_toPostgreSQL);
+
+var _ohmJs = require('ohm-js');
+
+var _ohmJs2 = _interopRequireDefault(_ohmJs);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 document.addEventListener('DOMContentLoaded', function () {
   var modelArea = document.getElementsByTagName('modelarea')[0],
-      modelAreaTextArea = modelArea.getElementsByTagName('textarea')[0],
-      errorArea = modelArea.getElementsByTagName('errorarea')[0],
-      resultArea = document.getElementsByTagName('resultarea')[0],
-      sqlArea = document.getElementsByTagName('sqlarea')[0],
-      sqlAreaTextArea = sqlArea.getElementsByTagName('textarea')[0],
-      importArea = document.getElementsByTagName('importarea')[0],
-      importAreaTextArea = importArea.getElementsByTagName('textarea')[0];
+      modelAreaTextArea = modelArea.getElementsByTagName('textarea')[0];
 
-  modelAreaTextArea.addEventListener('keyup', function (_ref) {
-    var target = _ref.target;
-    return a(target.value);
+  modelAreaTextArea.addEventListener('change', function (first, second, third) {
+    return console.log(first, second, third);
   });
-
-  a(modelAreaTextArea.value);
-
-  function a(value) {
-    try {
-      var _api = (0, _api2.api)(value, currentEngine, delimiter, quote);
-
-      var schema = _api.schema;
-      var imports = _api.imports;
-
-
-      sqlAreaTextArea.value = schema.join('\n') + '\n';
-      importAreaTextArea.value = imports.join('\n') + '\n';
-      errorArea.classList.remove('has-error');
-    } catch (e) {
-      console.log('error!', e);
-      if (e instanceof _api2.GrammarError) {
-        var match = e.match;
-
-        errorArea.classList.add('has-error');
-        errorArea.innerHTML = match.message;
-      } else {
-        errorArea.classList.add('has-error');
-        errorArea.innerHTML = e.message;
-      }
-    }
-  }
 });
+// [model, result, sql, import] = ["model", "result", "sql", "import"].map(n => document.getElementBy)
 
-function processModel(engine, model) {
-  return (0, _api2.api)(model, currentEngine, delimiter, quote);
+function loadGrammarWithSemantics(grammarName) {
+  var semanticNames = arguments.length <= 1 || arguments[1] === undefined ? [] : arguments[1];
+  var grammarText = arguments.length <= 2 || arguments[2] === undefined ? '' : arguments[2];
+
+  var grammar = _ohmJs2.default.grammars(grammarText)[grammarName],
+      semantics = grammar.semantics();
+
+  semanticNames.forEach(addSemanticName);
+
+  return { grammar: grammar, semantics: semantics };
+
+  function addSemanticName(name) {
+    var s = require('./grammar/' + grammarName + '.' + name + '.semantics').default;
+    semantics.addOperation(name, s);
+  }
 }
