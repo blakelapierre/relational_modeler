ab19ea75086eace68d03d42f36d4cdd544c58b9d
Blake La Pierre <blakelapierre@gmail.com>
2016-09-14 17:10:09 -0700

    Semantic cleanup

--- src/grammar/RM.toObject.semantics.js
+++ src/grammar/RM.toObject.semantics.js
@@ -4,92 +4,97 @@ const defaultType = 'text',
       defaultPrimaryKeyType = 'bigserial';
 
 export default {
-  ListOf_some (element, separator, rest) {
-    return prepend(element, rest);
-  },
+  ListOf_some:
+    (element, separator, rest) =>
+      prepend(element, rest),
 
-  ListOf_none () {
-    return undefined;
-  },
+  ListOf_none: () => undefined,
 
-  CContained (open, element, close) {
-    return single(element);
-  },
+  CContained:
+    (open, element, close) =>
+      single(element),
 
-  Model (name, commonAttributes, schemas) {
-    return join({name, commonAttributes: first(commonAttributes) || [], schemas});
-  },
+  Model:
+    (name, commonAttributes, schemas) =>
+      join({name, commonAttributes: first(commonAttributes) || [], schemas}),
 
-  Schema (name, commonAttributes, tables) {
-    return join({name, commonAttributes: first(commonAttributes) || [], tables});
-  },
+  Schema:
+    (name, commonAttributes, tables) =>
+      join({name, commonAttributes: first(commonAttributes) || [], tables}),
 
-  Table (name, attributes, dependencies) {
-    return join({name, attributes: first(attributes), dependencies});
-  },
+  Table:
+    (name, attributes, dependencies) =>
+      join({name, attributes: first(attributes), dependencies}),
 
-  Dependency (preArity, glyph, postArity, primaryKeyOrUnique, reference, optional, name) {
-    primaryKeyOrUnique = first(primaryKeyOrUnique) || {};
-    return Object.assign(join({
-      preArity: first(preArity) || '*',
-      postArity: first(postArity) || '*',
-      reference,
-      optional: !!first(optional),
-      name: first(name)
-    }), primaryKeyOrUnique);
-  },
+  Dependency:
+    (preArity, glyph, postArity, primaryKeyOrUnique, reference, optional, name) => {
+      primaryKeyOrUnique = first(primaryKeyOrUnique) || {};
+      return Object.assign(join({
+        preArity: first(preArity) || '*',
+        postArity: first(postArity) || '*',
+        reference,
+        optional: !!first(optional),
+        name: first(name)
+      }), primaryKeyOrUnique);
+    },
 
-  RegularAttribute (primaryKeyOrUnique, name, optional, type, check) {
-    primaryKeyOrUnique = first(primaryKeyOrUnique) || {};
-    type = first(type) || (primaryKeyOrUnique.primaryKey ? defaultPrimaryKeyType : defaultType);
-    return Object.assign(join({
-      name,
-      optional: !!first(optional),
-      type,
-      check: first(check) // using first() here is a little bit of a hack; I want check to be undefined if there is none specified, without calling first() it is an empty array
-    }), primaryKeyOrUnique);
-  },
+  RegularAttribute:
+    (primaryKeyOrUnique, name, optional, type, check) => {
+      primaryKeyOrUnique = first(primaryKeyOrUnique) || {};
+      type = first(type) || (primaryKeyOrUnique.primaryKey ? defaultPrimaryKeyType : defaultType);
+      return Object.assign(join({
+        name,
+        optional: !!first(optional),
+        type,
+        check: first(check) // using first() here is a little bit of a hack; I want check to be undefined if there is none specified, without calling first() it is an empty array
+      }), primaryKeyOrUnique);
+    },
 
   PrimaryKey:
-    primaryKey => join({primaryKey: first(primaryKey) === '@'}),
+    primaryKey =>
+      join({primaryKey: first(primaryKey) === '@'}),
 
   Unique:
-    unique => join({unique: first(unique) === '!'}),
+    unique =>
+      join({unique: first(unique) === '!'}),
 
   Optional:
-    optional => first(optional) === '?',
+    optional =>
+      first(optional) === '?',
 
-  Type (type) {
-    return single(type);
-  },
+  Type: single,
 
-  List (values) {
-    return join({type: 'List', values});
-  },
+  List:
+    values =>
+      join({type: 'List', values}),
 
-  Set (values) {
-    return join({type: 'Set', values});
-  },
+  Set:
+    values =>
+      join({type: 'Set', values}),
 
-  Constraint: (operator, value) => join({operator, value}),
+  Constraint:
+    (operator, value) =>
+      join({operator, value}),
 
-  Operator:
-    symbol => single(symbol),
+  Operator: single,
 
   CheckName:
-    name => join({check: 'Name', name}),
+    name =>
+      join({check: 'Name', name}),
 
   CheckNumber:
-    number => join({check: 'Number', number}),
+    number =>
+      join({check: 'Number', number}),
 
-  SchemaTableName (schema, dot, table) {
-    return join({schema, table});
-  },
+  SchemaTableName:
+    (schema, dot, table) =>
+      join({schema, table}),
 
-  TableName (table) {
-    return join({table});
-  },
+  TableName:
+    table =>
+      join({table}),
 
+  // Cannot use => syntax here as `this` must be preserved
   name (first_character, additional_characters) {
     return this.interval.contents;
   }
